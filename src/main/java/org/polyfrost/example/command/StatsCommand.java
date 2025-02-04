ce9ea91c6201af6548727b7de38ede20a996a8d0
Waffles <waffles3438@gmail.com>
2025-02-04 16:31:11 -0500

    prestige colors working

--- src/main/java/org/polyfrost/example/command/StatsCommand.java
+++ src/main/java/org/polyfrost/example/command/StatsCommand.java
@@ -44,7 +44,7 @@ public class StatsCommand {
         }
 
         JsonObject profile, ach, bw;
-        int star, fk, bb, w, ws;
+        int star, fk, bb, w;
         double fkdr, wlr, bblr;
         connection = newConnection("https://api.hypixel.net/player?key=" + ModConfig.api + "&uuid=" + uuid);
         profile = getStringAsJson(connection).getAsJsonObject("player");
@@ -95,7 +95,7 @@ public class StatsCommand {
         bblr = (double) Math.round(bblr * 100) / 100;
 
         UChat.chat("----------------------------------------------------");
-        UChat.chat("[" + star + "✫] " + color + Player);
+        UChat.chat(getFormattedRank(star) + " " + color + Player);
         UChat.chat("FKDR: " + fkdr);
         UChat.chat("Final kills: " + fk);
         UChat.chat("WLR: " + wlr);
@@ -153,5 +153,103 @@ public class StatsCommand {
             } catch (IOException e) { }
         }
         return "";
+    }
+
+    public enum Rank {
+        STONE1("§7[_✫]"),
+        STONE("§7[__✫]"),
+        IRON("§f[___✫]"),
+        GOLD("§6[___✫]"),
+        DIAMOND("§b[___✫]"),
+        EMERALD("§2[___✫]"),
+        SAPPHIRE("§3[___✫]"),
+        RUBY("§4[___✫]"),
+        CRYSTAL("§d[___✫]"),
+        OPAL("§9[___✫]"),
+        AMETHYST("§5[___✫]"),
+        RAINBOW("§c[§6_§e_§a_§b_§d✫§5]"),
+        IRON_PRIME("§7[§f____§7✪§7]"),
+        GOLD_PRIME("§7[§e____§6✪§7]"),
+        DIAMOND_PRIME("§7[§b____§3✪§7]"),
+        EMERALD_PRIME("§7[§a____§2✪§7]"),
+        SAPPHIRE_PRIME("§7[§3____§9✪§7]"),
+        RUBY_PRIME("§7[§c____§4✪§7]"),
+        CRYSTAL_PRIME("§7[§d____§5✪§7]"),
+        OPAL_PRIME("§7[§9____§1✪§7]"),
+        AMETHYST_PRIME("§7[§5____§8✪§7]"),
+        MIRROR("§8[§7_§f__§7_§8✪]"),
+        LIGHT("§f[_§e__§6_§l⚝§6]"),
+        DAWN("§6[_§f__§b_§3§l⚝§3]"),
+        DUSK("§5[_§d__§6_§e§l⚝§e]"),
+        AIR("§b[_§f__§7_§l⚝§8]"),
+        WIND("§f[_§a__§2_§l⚝§2]"),
+        NEBULA("§4[_§c__§d_§l⚝§d]"),
+        THUNDER("§e[_§f__§8_§l⚝§8]"),
+        EARTH("§a[_§2__§6_§l⚝§e]"),
+        WATER("§b[_§3__§9_§l⚝§1]"),
+        FIRE("§e[_§6__§c_§l⚝§4]");
+
+        private final String format;
+
+        Rank(String format) {
+            this.format = format;
+        }
+
+        public String getFormat() {
+            return format;
+        }
+    }
+
+    public String getFormattedRank(int star) {
+        if(star > 3000) {
+            return "§f[" + star + "✫]";
+        }
+        Rank rank = getRankForNumber(star);
+        String starString = String.valueOf(star);
+        StringBuilder txt = new StringBuilder(rank.getFormat());
+        int starCounter = 0;
+        for(int i = 0; i < txt.length(); i++){
+            if(rank.getFormat().charAt(i) == '_'){
+                txt.deleteCharAt(i);
+                txt.insert(i, starString.charAt(starCounter));
+                starCounter++;
+            }
+        }
+        return txt.toString();
+    }
+
+    private Rank getRankForNumber(int number) {
+        if (number < 10) return Rank.STONE1;
+        else if (number < 100) return Rank.STONE;
+        else if (number < 200) return Rank.IRON;
+        else if (number < 300) return Rank.GOLD;
+        else if (number < 400) return Rank.DIAMOND;
+        else if (number < 500) return Rank.EMERALD;
+        else if (number < 600) return Rank.SAPPHIRE;
+        else if (number < 700) return Rank.RUBY;
+        else if (number < 800) return Rank.CRYSTAL;
+        else if (number < 900) return Rank.OPAL;
+        else if (number < 1000) return Rank.AMETHYST;
+        else if (number < 1100) return Rank.IRON_PRIME;
+        else if (number < 1200) return Rank.GOLD_PRIME;
+        else if (number < 1300) return Rank.DIAMOND_PRIME;
+        else if (number < 1400) return Rank.EMERALD_PRIME;
+        else if (number < 1500) return Rank.SAPPHIRE_PRIME;
+        else if (number < 1600) return Rank.RUBY_PRIME;
+        else if (number < 1700) return Rank.CRYSTAL_PRIME;
+        else if (number < 1800) return Rank.OPAL_PRIME;
+        else if (number < 1900) return Rank.AMETHYST_PRIME;
+        else if (number < 2000) return Rank.MIRROR;
+        else if (number < 2100) return Rank.LIGHT;
+        else if (number < 2200) return Rank.DAWN;
+        else if (number < 2300) return Rank.DUSK;
+        else if (number < 2400) return Rank.AIR;
+        else if (number < 2500) return Rank.WIND;
+        else if (number < 2600) return Rank.NEBULA;
+        else if (number < 2700) return Rank.THUNDER;
+        else if (number < 2800) return Rank.EARTH;
+        else if (number < 2900) return Rank.WATER;
+        else if (number < 3000) return Rank.FIRE;
+        return Rank.RAINBOW;
     }
 }
